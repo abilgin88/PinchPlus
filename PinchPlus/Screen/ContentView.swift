3007a004c630f9791e3445874e09f732d7a74d7b
Abdullah Bilgin <abilgin02@icloud.com>
2023-07-23 21:24:56 +0300

    Double Tap Gesture

--- PinchPlus/Screen/ContentView.swift
+++ PinchPlus/Screen/ContentView.swift
@@ -8,19 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var isAnimation: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.1), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimation ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimation)
+                    .scaleEffect(imageScale)
+                // MARK: 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                isAnimation = true
+            }
+            )
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
