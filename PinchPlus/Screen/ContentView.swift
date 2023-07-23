4c238701da2dcbfc54dc08ab33f6a867d92a7df2
Abdullah Bilgin <abilgin02@icloud.com>
2023-07-23 22:42:03 +0300

    Drag Gesture

--- PinchPlus/Screen/ContentView.swift
+++ PinchPlus/Screen/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
     
     @State private var isAnimation: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero // CGSize(width: 0, height: 0)
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
@@ -28,6 +36,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.1), radius: 12, x: 2, y: 2)
                     .opacity(isAnimation ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimation)
                     .scaleEffect(imageScale)
                 // MARK: 1. TAP GESTURE
@@ -37,11 +46,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
